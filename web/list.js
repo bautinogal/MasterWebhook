084d1f6984baaae87f68a8b80b813ba4ba5e9159
bautinogal <jbnogal@gmail.com>
2020-06-09 04:51:47 -0300

    list()

--- web/list.js
+++ web/list.js
@@ -3,12 +3,12 @@ const repo = require('../lib/repo');
 
 const listRoute = (req, res) => {
     console.log("GET POSTS FROM: " + req.protocol + '://' + req.get('host') + req.originalUrl);
-    console.log(JSON.stringify(repo.list().messages));
+    console.log(JSON.stringify(repo.list()));
     repo
         .list()
         .then(messages => {
             res.setHeader('content-type', 'application/json');
-            res.end(JSON.stringify(messages));
+            res.end(JSON.stringify(repo.list()));
         })
         .catch(e => {
             console.error(e);
